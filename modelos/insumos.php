bd11afd8e119ca9b187e910ac6294cb01efa674a
paola <pebitaa5795@hotmail.com>
2023-05-22 08:15:41 -0600

    se crea el html para mostrar guardar o error

---
+++ modelos/insumos.php
@@ -0,0 +1,22 @@
+<?php
+require 'Conexion.php';
+// se crea la conexion a bd 
+class Insumo extends Conexion{
+    public $insumos_id;
+    public $insumos_nombre;
+    public $insumos_existencia;
+    public $insumos_marca;
+    public $insumos_proveedor;
+    public $insumos_precio;
+
+    public function __construct($args = [] )
+    {
+        $this->insumos_id = $args['insumos_id'] ?? null;
+        $this->insumos_nombre = $args['insumos_nombre'] ?? '';
+        $this->insumos_existencia = $args['insumos_existencia'] ?? '';
+        $this->insumos_marca = $args['insumos_marca'] ?? '';
+        $this->insumos_proveedor = $args['insumos_proveedor'] ?? '';
+        $this->insumos_precio = $args['insumos_precio'] ?? '';
+    }
+
+}
